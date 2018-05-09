204bf46c7f71606c4222b53a8b26cc2843817662
aliyang <aliyang@domain.com>
2018-05-09 16:16:07 +0800

    update

---
+++ src/T_38_2.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author aliyang
+ * @date 18-5-9 下午4:13
+ * 二叉树的深度：别人解法
+ * 思路：层序遍历
+ */
+public class T_38_2 {
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+
+    public int TreeDepth(TreeNode pRoot)
+    {
+        if(pRoot == null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(pRoot);
+        int depth = 0, count = 0, nextCount = 1;
+        while(queue.size()!=0){
+            TreeNode top = queue.poll();
+            count++;
+            if(top.left != null){
+                queue.add(top.left);
+            }
+            if(top.right != null){
+                queue.add(top.right);
+            }
+            if(count == nextCount){
+                nextCount = queue.size();
+                count = 0;
+                depth++;
+            }
+        }
+        return depth;
+    }
+}
