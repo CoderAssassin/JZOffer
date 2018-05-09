847e39005eb5652cc31fd7c12e627be69b7ce5e1
aliyang <aliyang@domain.com>
2018-05-09 16:09:54 +0800

    update

---
+++ src/T_38_1.java
@@ -0,0 +1,24 @@
+/**
+ * @author aliyang
+ * @date 18-5-9 下午4:07
+ * 二叉树的深度：别人解法
+ * 最短的递归
+ */
+public class T_38_1 {
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    public int TreeDepth(TreeNode root) {
+
+    }
+}
