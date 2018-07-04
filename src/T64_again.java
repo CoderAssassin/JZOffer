671a2c664a26924f57de761d80aedc4110e6d377
aliyang <442829392@qq.com>
2018-07-04 17:01:11 +0800

    update

--- src/T64_again.java
+++ src/T64_again.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 /**
@@ -7,39 +8,66 @@ import java.util.ArrayList;
  **/
 public class T64_again {
 
+//    public ArrayList<Integer> maxInWindows(int [] num, int size)
+//    {
+//        ArrayList<Integer> res=new ArrayList<>();
+//        if (num.length==0||size==0||size>num.length)
+//            return res;
+//        int max=Integer.MIN_VALUE,maxIndex=-1,sum=0,i=0;
+//        for (;i<num.length&&i<size;i++){
+//            if (num[i]>max){
+//                max=num[i];
+//                maxIndex=i;
+//            }
+//        }
+//        res.add(max);
+//
+//        int first=0;//窗口的起始位置
+//        for (;i<num.length;i++){
+//            first++;
+//            if (num[i]>=max){//如果新的数更大，直接标为新的数
+//                max=num[i];
+//                maxIndex=i;
+//            }else {//如果最大的数过期了，那么重新选取
+//                if (maxIndex<first){
+//                    max=Integer.MIN_VALUE;
+//                    for (int j=first;j<first+size;j++){
+//                        if (num[j]>max){
+//                            max=num[j];
+//                            maxIndex=j;
+//                        }
+//                    }
+//                }
+//            }
+//            res.add(max);
+//        }
+//        return res;
+//    }
+
+//    思路二：双端队列
     public ArrayList<Integer> maxInWindows(int [] num, int size)
     {
         ArrayList<Integer> res=new ArrayList<>();
-        if (num.length==0||size==0||size>num.length)
+        if (size==0)
             return res;
-        int max=Integer.MIN_VALUE,maxIndex=-1,sum=0,i=0;
-        for (;i<num.length&&i<size;i++){
-            if (num[i]>max){
-                max=num[i];
-                maxIndex=i;
-            }
-        }
-        res.add(max);
+        ArrayDeque<Integer> deque=new ArrayDeque<>();
+        int begin;
+        for (int i=0;i<num.length;i++){
+            begin=i-size+1;
+            if (deque.isEmpty())
+                deque.add(i);
+            else if (deque.peekFirst()<begin)
+                deque.pollFirst();
 
-        int first=0;//窗口的起始位置
-        for (;i<num.length;i++){
-            first++;
-            if (num[i]>=max){//如果新的数更大，直接标为新的数
-                max=num[i];
-                maxIndex=i;
-            }else {//如果最大的数过期了，那么重新选取
-                if (maxIndex<first){
-                    max=Integer.MIN_VALUE;
-                    for (int j=first;j<first+size;j++){
-                        if (num[j]>max){
-                            max=num[j];
-                            maxIndex=j;
-                        }
-                    }
-                }
-            }
-            res.add(max);
+            while ((!deque.isEmpty())&&num[deque.peekLast()]<=num[i])
+                deque.pollLast();
+
+            deque.add(i);
+
+            if (begin>=0)
+                res.add(num[deque.peekFirst()]);
         }
+
         return res;
     }
 
